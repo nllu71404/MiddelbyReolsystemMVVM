d3cbfaf1ac9d1ea03816bec1d32561ab0f30dc1b
Lauraerunik <168111337+Lauraerunik@users.noreply.github.com>
2025-09-23 15:23:58 +0200

    Ændret tilbage til views, miv :(

---
+++ MiddelbyReolsystemMVVM/Views/RackOverviewPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MiddelbyReolsystemMVVM.Services;
+using MiddelbyReolsystemMVVM.Viewmodels;
+
+namespace MiddelbyReolsystemMVVM.Views
+{
+    public partial class RackOverviewPage : Page
+    {
+        private INavigationService? _nav;
+
+        public RackOverviewPage()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded; 
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (_nav != null) return; // kør kun én gang
+
+            _nav = new PageNavigationService(this.NavigationService);
+            //_nav.Register("AdminRenterPage", () => new AdminRenterPage());
+            //_nav.Register("AdminRackPage", () => new AdminRackPage());
+
+            // DataContext = new RackViewModel(new RackService(), _nav);
+        }
+    }
+}
